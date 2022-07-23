f23151e793a8fd8d5b1c889f8389883f8876a156
Digvijay Patil <43775445+DigvijayPati@users.noreply.github.com>
2022-07-23 13:36:34 +0200

    Delete zap.bat

--- dast/zap.bat
+++
@@ -1,7 +0,0 @@
-if exist "%USERPROFILE%\OWASP ZAP\.ZAP_JVM.properties" (
-	set /p jvmopts=< "%USERPROFILE%\OWASP ZAP\.ZAP_JVM.properties"
-) else (
-	set jvmopts=-Xmx512m
-)
-
-java %jvmopts% -jar zap-2.11.1.jar %*
